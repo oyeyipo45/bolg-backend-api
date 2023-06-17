2099de502043910cfb55a647bb1673b1e80c4672
Damilola Oyeyipo <oyeyipo45@gmail.com>
2023-06-17 13:20:01 +0100

    Initial Commit

---
+++ src/test/java/com/damilola/blogbackendapi/BlogBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damilola.blogbackendapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogBackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
