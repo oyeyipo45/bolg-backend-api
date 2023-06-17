94aaa22ce06580e8613344611a16f9e4419c1448
Damilola Oyeyipo <oyeyipo45@gmail.com>
2023-06-17 13:48:51 +0100

    Create default rest endpoint

--- src/main/java/com/damilola/blogbackendapi/controller/HelloWorldContoller.java
+++ src/main/java/com/damilola/blogbackendapi/controller/HelloWorldContoller.java
@@ -1,9 +1,14 @@
 package com.damilola.blogbackendapi.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @ResponseBody
 public class HelloWorldContoller {
+    @GetMapping("/hello-world")
+    public String helloworld(){
+        return "Hello world";
+    }
 }
