9064babf3918cc3aa5f0e40ee289d9545e886fae
Damilola Oyeyipo <oyeyipo45@gmail.com>
2023-06-19 23:47:16 +0100

    Add response entity for all mappings and refactor

--- src/main/java/com/damilola/blogbackendapi/controller/StudentController.java
+++ src/main/java/com/damilola/blogbackendapi/controller/StudentController.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("students")
 public class StudentController {
     @GetMapping("student")
     public ResponseEntity<Student> getStudent(){
@@ -18,7 +19,7 @@ public class StudentController {
     }
 
 
-    @GetMapping("students")
+    @GetMapping
     public ResponseEntity<List<Student>> getStudents(){
         List<Student> students = new ArrayList<>();
         students.add(new Student(1,  "damilila", "oyeyipo"));
@@ -27,35 +28,35 @@ public class StudentController {
        return new ResponseEntity<>(students, HttpStatus.OK);
     }
 
-    @GetMapping("students/{id}")
+    @GetMapping("{id}")
     public ResponseEntity<Student> oneStudent (@PathVariable("id") int studentId) {
         Student student = new Student(studentId, "Gbemi", "Oye");
         return new ResponseEntity<>(student,  HttpStatus.OK);
     }
 
-    @GetMapping("students/query")
+    @GetMapping("query")
     public ResponseEntity<Student> studentRequest (@RequestParam int id,
                                    @RequestParam String name) {
         Student student = new Student(id, name, "Oye");
         return new ResponseEntity<>(student,  HttpStatus.OK);
     }
-    @PostMapping("students")
+    @PostMapping("")
     public ResponseEntity<Student> createStudent (@RequestBody Student student) {
             return new ResponseEntity<>(student, HttpStatus.CREATED);
     }
 
-    @PutMapping("students/{id}")
+    @PutMapping("{id}")
     public ResponseEntity<Student> updateStudent (@RequestBody Student student,
                                   @PathVariable int id) {
 
         Student firstStudent = new Student(1, "Dami",  "oye");
         firstStudent.setFirstName("damilola");
-        firstStudent.setId(3);
+        firstStudent.setId(id);
 
         return new ResponseEntity<>(firstStudent, HttpStatus.OK);
     }
 
-    @DeleteMapping("students/{id}")
+    @DeleteMapping("{id}")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<String> updateStudent (@PathVariable int id) {
         return new ResponseEntity<>("Student deleted", HttpStatus.OK);
